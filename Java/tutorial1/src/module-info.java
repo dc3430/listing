55413583adafa8930ea331d649b15acb98bdfa28
Daniel <cermak222@gmail.com>
2019-11-07 17:03:56 -0600

    java objects

---
+++ Java/tutorial1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dc
+ *
+ */
+module tutorial1 {
+}
---
+++ Java/tutorial1/src/tutorial1/Main.java
@@ -0,0 +1,21 @@
+package tutorial1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//primitive 
+		int spam_yum = 9;
+		double num2 = 5.0;
+		boolean b = true;
+		char c = '7';
+		//not primitive
+		String str = "tim_*";
+		
+		System.out.println(spam_yum);
+		System.out.println(spam_yum + 4);
+		System.out.println(c/num2);
+	}
+
+}
+
