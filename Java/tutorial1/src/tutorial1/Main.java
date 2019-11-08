d10db0324c328fdbf892617b863e61f09efd5bbe
Daniel <cermak222@gmail.com>
2019-11-08 16:23:33 -0600

    writting java

--- Java/tutorial1/src/tutorial1/Main.java
+++ Java/tutorial1/src/tutorial1/Main.java
@@ -1,4 +1,5 @@
 package tutorial1;
+import java.util.Scanner;
 
 public class Main {
 
@@ -15,7 +16,43 @@ public class Main {
 		System.out.println(spam_yum);
 		System.out.println(spam_yum + 4);
 		System.out.println(c/num2);
+		
+		//operators 
+		int x = 5;
+		int y = 7;
+		int z = 56;
+		int u = z/y;
+		double s = 72;
+		double t = s/x;
+		int sum = x + y + z;
+		int mut = x * y * z;
+		int sub = x + y - z;
+		double d = Math.pow(x, y);
+		
+		System.out.println(sum);
+		System.out.println(mut);
+		System.out.println(sub);
+		System.out.println(u);
+		System.out.println(t);
+		System.out.println(d);
+		
+		//type casting changing the declared variable in line
+		int f = 3;
+		int e = 8;
+		double i = f / (double)e; 
+		System.out.println(i);
+		
+		//remainder give you the division of an operation 
+		int q = 56 % 5; // 11 r 1
+		
+		System.out.println(q);
+		
+		//Input
+		Scanner sc = new Scanner(System.in);
+		String scanned = sc.next();
+		int p = Integer.parseInt(scanned);
+		
+		System.out.println(p);
 	}
 
 }
-
