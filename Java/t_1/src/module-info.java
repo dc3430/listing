dde9352ac46ad86963207c5de5fc945676037ab8
Daniel <cermak222@gmail.com>
2019-11-20 20:04:11 -0600

    add if

--- Java/t_1/src/module-info.java
+++ Java/t_1/src/module-info.java
@@ -1,2 +1,13 @@
 module t_1 {
+		  public static void main(String[] args) {
+		    int time = 20;
+		    if (time < 18) {
+		      System.out.println("Good day.");
+		    } else {
+		      System.out.println("Good evening.");
+		    }  
+		  }
+		}
+
 }
+
